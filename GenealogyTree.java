d006a45ebca387677f8a0242d63458d91bf57a31
Joong Ho Kim <jkim574@wisc.edu>
2017-11-19 21:57:07 -0600

    changes

--- GenealogyTree.java
+++ GenealogyTree.java
@@ -2,13 +2,13 @@ import java.util.*;
 import java.io.*;
 
 /**
- * A general tree that is can be used to keep 
+ * A general tree that is can be used to keep
  * track of parent child relationships between data.
- * 
+ *
  * Nodes in a general tree can have multiple children.
- * 
+ *
  * This tree is built by reading lines from a file.
- * 
+ *
  * To help with program 4, this class also has a method
  * that returns a stack of String (names) that are the
  * ancestors of the specified node.  See getAncestorStack.
@@ -35,7 +35,7 @@ public class GenealogyTree{
 	 * and the stack contains all ancestors of the matching data node.
 	 *
 	 * THIS METHOD CALLS A COMPANION (HELPER) METHOD that is recursive
-	 * 
+	 *
 	 * If the top of the stack is not target,
 	 * return an empty stack indicating that the target
 	 * was not found.
@@ -55,22 +55,40 @@ public class GenealogyTree{
 	}
 
 	/**
-	 * Perform a pre-order traversal of the current node and 
+	 * Perform a pre-order traversal of the current node and
 	 * return a Stack that contains the ancestors of the target node.
-	 * 
+	 *
 	 * The root is at the bottom of the stack and the matching node is at the top
 	 * and the stack contains all and only ancestors of the matching data node.
-	 * 
+	 *
 	 * NOTE: If target data is not found, the stack returned does not have
 	 * target at the top.  Be sure to check this in the calling method.
 	 *
 	 * @param target the data you are trying to find
-	 * @return a stack with the target data node at top and the root at the bottom 
+	 * @return a stack with the target data node at top and the root at the bottom
 	 * or an empty stack if target is not found
 	 */
 	private StackADT<String> getAncestorStack(StackADT<String> st, TreeNode<String> curr, String target) {
 		// TODO: must implement this method
-		
+	    if (curr != null) {
+	        st.push();
+		if (st.peek().equals(children)) {
+		    return st;
+		} else {
+		    root.getChildren();
+		}
+		for (String cur : st) {
+		    root.getChildren();
+		    if (root.addChild(target)) {
+			return st;
+		    } else {
+			st.pop();
+		    }
+		}
+		return st;
+
+	    }
+	}
 		// if current node is not null
 			// push to stack
 			// check for match
@@ -84,15 +102,15 @@ public class GenealogyTree{
 				// if top of ancestry stack equals target
 					// return stack
 				// otherwise, pop the top from the stack
-		
+
 		// return stack if done processing ch
 	}
 
 	/**
 	 * Load a tree from file.
 	 *
-	 * If there are IOException when loading the tree from file, 
-	 * print LOAD_GENEALOGY_ERROR_MESSAGE 
+	 * If there are IOException when loading the tree from file,
+	 * print LOAD_GENEALOGY_ERROR_MESSAGE
 	 * and throw the IOException.
 	 *
 	 * All the lines in the file "parent -> children" relationships.
@@ -116,9 +134,9 @@ public class GenealogyTree{
 	 *
 	 * Note: all lines of a file must contain a relationship to be a valid format.
 	 * Blank lines will cause exceptions.
-	 * 
+	 *
 	 * Pseudocode for the work done by this method:
-	 * 
+	 *
 	 * 	// Create a queue, add each new node to the queue
 	 *	// create a Scanner connect to the file
 	 *  // for each line of the file
@@ -138,15 +156,32 @@ public class GenealogyTree{
 	 *                  // add the child node to the current "front" node (its parent)
 	 *                  // add the child to the queue
 	 *                  // break out of the loop
-	 *              	// else dequeue the front node 
+	 *              	// else dequeue the front node
 	 *
 	 *  // catch IO exceptions, display error message and rethrow the exception
 	 *	// close the file scanner
-	 * 
+	 *
 	 */
 	public void buildFromFile(String filename) throws IOException{
         // TODO: COMPLETE THIS METHOD
+	    QueueADT<String> queue = new QueueADT<String>();
+	    String line;
+	    String[] parts;
+	    for (String s : queue) {
+		Scanner sc = new Scanner(filename);
+		while (in.hasNext()) {
+		    line = in.nextLine();
+		    line = line.trim();
+		    for (String s : parts) {
 
+		    }
+		    if (root == null) {
+			root.addChild(line);
+
+
+		    }
+		}
+	    }
 		// Create a queue, add each new node to the queue
 			// create a Scanner connect to the file
 
@@ -157,13 +192,13 @@ public class GenealogyTree{
 				// parse the line into parent and child
 
 				// if root is null
-		
+
 					// create the root
 
 					// add its first child
 
 					// add the root and child to the queue
-		
+
 				// else Construct other TreeNode
 
 					// while queue is not empty
@@ -180,22 +215,22 @@ public class GenealogyTree{
 
 							// break out of the loop
 
-						// else dequeue the front node 
+						// else dequeue the front node
 
 			// catch IO exceptions, display error message and rethrow the exception
 
 			// close the file scanner
-	}            
+	}
 
 	/**
 	 * Display the contents of the tree in a horizontal tree form.
-	 * 
+	 *
 	 * This method is a private recursive helper method for the
 	 * printTree() method.
-	 * 
-	 * It uses the indentation levels to indicate how many 
+	 *
+	 * It uses the indentation levels to indicate how many
 	 * dots (two per each level) to print for the node
-	 * 
+	 *
 	 * @param current node to print
 	 * @param indent_count indicates how many dots .. to print for the current level
 	 * @param indent_str indicates string of characters precede each print level
